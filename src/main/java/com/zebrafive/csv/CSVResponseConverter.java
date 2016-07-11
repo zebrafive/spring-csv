0189c878979ab6c1c2bf9c79bc01a27bc0bcdc68
zebrafive <jason@jasonhooker.com>
2016-07-12 08:13:10 +1000

    Base implementation for CSV response converter.

--- src/main/java/com/zebrafive/csv/CSVResponseConverter.java
+++ src/main/java/com/zebrafive/csv/CSVResponseConverter.java
@@ -1,4 +1,41 @@
 package com.zebrafive.csv;
 
-public class CSVResponseConverter {
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class CSVResponseConverter extends AbstractHttpMessageConverter<CSVResponse> {
+    
+    public CSVResponseConverter() {
+        super(new MediaType("text", "comma-separated-values"));
+    }
+
+    @Override
+    protected boolean supports(Class<?> realClass) {
+        return CSVResponse.class.equals(realClass);
+    }
+
+    @Override
+    protected CSVResponse readInternal(Class<? extends CSVResponse> realClass, HttpInputMessage inputMessage)
+            throws IOException {
+                
+        return null;
+    }
+
+    @Override
+    protected void writeInternal(CSVResponse csvResponse, HttpOutputMessage outputMessage)
+            throws IOException {
+                
+        try (CSVWriter writer = new CSVWriter(new OutputStreamWriter(outputMessage.getBody(), StandardCharsets.UTF_8))) {
+            writer.writeNext(csvResponse.getFieldNames());
+            writer.writeAll(csvResponse.getAllRecords());
+        }
+    }
 }
