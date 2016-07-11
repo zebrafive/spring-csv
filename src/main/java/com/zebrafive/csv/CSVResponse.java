5639d7d3fa5d02c3a07cd745272d040b7fb9944c
zebrafive <jason@jasonhooker.com>
2016-07-12 08:12:31 +1000

    Added methods to retrieve field names and all records. Currently returns test data only.

--- src/main/java/com/zebrafive/csv/CSVResponse.java
+++ src/main/java/com/zebrafive/csv/CSVResponse.java
@@ -1,4 +1,16 @@
 package com.zebrafive.csv;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class CSVResponse {
+    
+    public String[] getFieldNames() {
+        return new String[]{"test1", "test2"};
+    }
+    
+    public List<String[]> getAllRecords() {
+        List<String[]> allRecords = new ArrayList<>();
+        allRecords.add(new String[]{"value1", "value2"});
+    }
 }
