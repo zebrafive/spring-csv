5a474b4d1f49219f573a58b56cbcff70de193569
zebrafive <jason@jasonhooker.com>
2016-07-12 08:39:11 +1000

    Base implementation for csv response.

--- src/main/java/com/zebrafive/csv/CSVResponse.java
+++ src/main/java/com/zebrafive/csv/CSVResponse.java
@@ -1,17 +1,59 @@
 package com.zebrafive.csv;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CSVResponse {
-    
-    public String[] getFieldNames() {
-        return new String[]{"test1", "test2"};
+
+    private List<String> fieldNames;
+    private List<CSVRecord> csvRecords;
+
+    public CSVResponse() {
+        fieldNames = new ArrayList<>();
+        csvRecords = new ArrayList<>();
     }
-    
+
+    public String[] getFieldNames() {
+        return fieldNames.toArray(new String[fieldNames.size()]);
+    }
+
     public List<String[]> getAllRecords() {
-        List<String[]> allRecords = new ArrayList<>();
-        allRecords.add(new String[]{"value1", "value2"});
-        return allRecords;
+        return csvRecords.stream()
+                .map(CSVRecord::getFieldValues)
+                .collect(Collectors.toList());
+    }
+
+    public CSVRecord addRecord() {
+        CSVRecord record = new CSVRecord();
+        csvRecords.add(record);
+        return record;
+    }
+
+    public class CSVRecord {
+
+        private Map<String, Object> fieldValues = new HashMap<>();
+
+        public String[] getFieldValues() {
+            return (String[]) fieldNames.stream()
+                    .map(fieldName -> fieldValues.get(fieldName) != null ? fieldValues.get(fieldName).toString() : "")
+                    .toArray();
+        }
+
+        public CSVRecord withField(String fieldName, Object fieldValue) {
+            if (!fieldNames.contains(fieldName)) {
+                fieldNames.add(fieldName);
+            }
+
+            fieldValues.put(fieldName, fieldValue);
+
+            return this;
+        }
+
+        public CSVRecord andAddRecord() {
+            return addRecord();
+        }
     }
 }
